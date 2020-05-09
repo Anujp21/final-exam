35750e17295cc7c36bb09932464c3588063056c9
anu <anujp.jp@gmail.com>
2020-05-09 10:36:13 -0700

    step3:not a number check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'finalexam';
+
+
+  number1: number;
+  number2: number;
+  errors: string;
+  results: string;
+
+
+  validate(){
+    this.errors = ''
+    //Validate the numbers 
+    if(isNaN(this.number1)){
+      this.errors = 'Number 1 input '+this.number1+ ' is not a valid number\r\n';
+    }
+    if(isNaN(this.number2)){
+      this.errors = this.errors +  'Number 2 input '+this.number2+ ' is not a valid number';
+    }
+
+
+
+    return this.errors;
+  }
+
+  calculate(){
+    console.log('calculating');
+    this.errors = '';
+    this.results = '';
+    this.validate();
+
+
+  }
+
+
+
+
 }
